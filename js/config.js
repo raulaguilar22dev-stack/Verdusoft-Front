39b75f45f610a6a5e7932612ad6bd226cfd9aeca
Raul Ricardo Aguilar <raul.aguilar.22.dev@gmail.com>
2026-06-29 23:12:04 -0300

    puede que arregle el login xd

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ const CONFIG = {
   API_BASE_URL:
     window.VERDUSOFT_API_URL || "https://verdusoft-backend.onrender.com/api",
   SUPABASE_URL: "https://lscmcxxvayzdgwinpokx.supabase.co",
-  SUPABASE_ANON_KEY: "813d8465-49e4-44f7-886b-3e4b65731096",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzY21jeHh2YXl6ZGd3aW5wb2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MDQ2MjEsImV4cCI6MjA5ODA4MDYyMX0.WLX85YGgwc8fWSpJbwPj-6-Fvy5ygsAC8fiFaosujMg",
   ITEMS_PER_PAGE: 20,
 };
